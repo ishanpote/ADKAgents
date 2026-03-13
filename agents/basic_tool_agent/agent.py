adc4c14154e522ea8fc16e772e2ac58b6db90460
ishanpote <160637054+ishanpote@users.noreply.github.com>
2026-03-13 13:56:34 +0530

    agents folder

---
+++ agents/basic_tool_agent/agent.py
@@ -0,0 +1,27 @@
+from google.adk.agents import Agent
+from google.adk.models.lite_llm import LiteLlm
+from tools.date_tool import get_current_datetime
+
+
+
+MODEL = "ollama/llama3"
+
+root_agent = Agent(
+    name="basic_agent",
+    model = "gemini-2.0-flash",
+    instruction="""
+        You are a helpful and friendly assistant.
+        Greet the user warmly when the conversation begins.
+        Respond to user questions in a polite and conversational way.
+        
+        If the user asks for the date or time, call the 'get_current_datetime' tool.
+    """,
+    tools=[get_current_datetime],
+)
+
+# Run a REPL-style chat
+if __name__ == "__main__":
+    while True:
+        user_input = input("You: ")
+        response = root_agent.chat(user_input)
+        print("Agent:", response)
